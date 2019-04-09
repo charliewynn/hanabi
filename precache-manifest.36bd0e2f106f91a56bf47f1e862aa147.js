a075ebf5e0502ffbd54ddb744be841a8bd3f376f
Charlie Wynn <charlie.wynn@technivation.com>
2019-04-09 17:50:11 -0500

    update to remove vulns and change name

---
+++ precache-manifest.36bd0e2f106f91a56bf47f1e862aa147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1faebd6c3a7d043a80b1",
+    "url": "/hanabi/static/js/runtime~main.99504b32.js"
+  },
+  {
+    "revision": "acecee08cdf3d028ab21",
+    "url": "/hanabi/static/js/main.57d851a5.chunk.js"
+  },
+  {
+    "revision": "79e02651ad6c949f7843",
+    "url": "/hanabi/static/js/2.1c1522b7.chunk.js"
+  },
+  {
+    "revision": "acecee08cdf3d028ab21",
+    "url": "/hanabi/static/css/main.ac637901.chunk.css"
+  },
+  {
+    "revision": "79e02651ad6c949f7843",
+    "url": "/hanabi/static/css/2.ffb81d23.chunk.css"
+  },
+  {
+    "revision": "ef96d42ee4d9f2e22b996b83292c094c",
+    "url": "/hanabi/index.html"
+  }
+];
